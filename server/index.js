3c96dc9281afcf7faa6acf660acbed1321c765e5
Luis Huertas <hu3rtas@gmail.com>
2020-07-11 23:43:15 -0500

    upload commit

--- server/index.js
+++ server/index.js
@@ -19,51 +19,7 @@ tinyfy.key = config.develoment.tinyKey;
 const app = express();
 
 const log = config.develoment.log();
-/*
-const storageImages = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './server/uploads/images');
-  }, 
-  filename: (req, file, cb) => {
-    const extension = file.originalname.split('.')[1];
-    cb(null, file.fieldname + '-' + req.params.codigo + '.' + extension);
-  }
-});
 
-const storagePDF = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './server/uploads/files');
-  }, 
-  filename: (req, file, cb) => {
-    cb(null,  'ficha-' + req.params.codigo + '.' + 'pdf');
-  }
-});
-
-const uploadImage = multer({
-  storage: storageImages,
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      cb(new Error('Please upload JPG and PNG images only!'));
-    }
-    cb(undefined, true);
-  }
-});
-
-const uploadPDF = multer({
-  storage: storagePDF,
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(pdf)$/)) {
-      cb(new Error('Please upload PDF only!'));
-    }
-    cb(undefined, true);
-  }
-});*/
 
 app.use(helmet());
 app.use(compression());
@@ -94,40 +50,6 @@ app.use('/inventario', inventarioRoutes);
 app.use('/auth', authRoutes);
 app.use('/ventas', ventasRoutes);
 
-/*
-app.post('/uploads/image/:codigo', uploadImage.single('img'), (req, res, next) => {
-    const file = req.file;
-    console.log(file);
-    console.log(file.path);
-    tinyfy.fromFile(file.path).toFile(file.path);
-    if (!file) {
-        const error = new Error('Please upload a file');
-        error.httpStatusCode = 400;
-        return next(error);
-    }
-    res.status(200).send({
-        statusCode: 200,
-        status: 'success',
-        uploadedFile: file
-    });
-});
-
-app.post('/uploads/ficha/:codigo', uploadPDF.single('pdf'), (req, res, next) => {
-  const file = req.file;
-  console.log(file);
-  console.log(file.path);
-  if (!file) {
-      const error = new Error('Please upload a file');
-      error.httpStatusCode = 400;
-      return next(error);
-  }
-  res.status(200).send({
-      statusCode: 200,
-      status: 'success',
-      uploadedFile: file
-  });
-});
-*/
 app.use('/static', express.static(path.join(__dirname, 'uploads')));
 
 
