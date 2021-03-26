f8c73d3c98f1be2f20127b40fd18f9027292673b
Luis Huertas <hu3rtas@gmail.com>
2021-03-26 09:39:37 -0500

    ..

--- server/controllers/nubeFactController.js
+++ server/controllers/nubeFactController.js
@@ -211,6 +211,7 @@ export const generarComprobante = (req, res, next) => {
         .then(resT => resT.json())
         .then(json => {
             req.sunat = json;
+            console.log(json);
             next();
         })
         .catch(err =>
